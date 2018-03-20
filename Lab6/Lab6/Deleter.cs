f51dcefa549db818ad043b74898d05b828aa43e3
Rise <ritm-temp@hotmail.com>
2018-03-20 10:49:35 +0600

    Lab6 v1.0

---
+++ Lab6/Lab6/Deleter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab6
+{
+    class Deleter
+    {
+        public List<int> Delete(List<int> array)
+        {
+            int difference = DifferenceBetweenFirstAndLast(array);
+            for (int i = 0; i < array.Count(); i++)
+            {
+                if (array[i] % difference == 0)
+                {
+                    array.RemoveAt(i);
+                    i--;
+                }
+            }
+            return array;
+        }
+
+        public int DifferenceBetweenFirstAndLast(List<int> array)
+        {
+            int difference = Math.Abs(Math.Abs(array[0]) - Math.Abs(array.Last()));
+            return difference;
+        }
+    }
+}
---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int> { 2, 3, 4, 5, -1, -2, -3, -4 };
+            foreach (int element in array)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+            Deleter deleter = new Deleter();
+            List<int> resultArray = deleter.Delete(array);
+            foreach (int element in resultArray)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.Read();
+        }
+    }
+}
